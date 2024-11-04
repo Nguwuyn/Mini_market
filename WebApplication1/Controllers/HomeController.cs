de96040ba035322dcb4a7b82f79b3adaa64d9f15
Nguwuyn <22dh114652@st.huflit.edu.vn>
2024-11-04 14:31:31 +0700

    cloning

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,29 +1,91 @@
-﻿using System;
+﻿using anhemtoicodeweb.Models;
+using anhemtoicodeweb.Models.ThongKe;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
+using System.Text;
 using System.Web.Mvc;
 
-namespace WebApplication1.Controllers
+namespace anhemtoicodeweb.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly Model1 database = new Model1();
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = database.Products.OrderByDescending(x => x.NamePro).ToList();
+            IEnumerable<Category> categoriesList = database.Categories.OrderByDescending(x => x.NameCate).ToList();
+            var tuple = new Tuple<IEnumerable<Product>, IEnumerable<Category>>(productList, categoriesList);
+            return View(tuple);
         }
 
-        public ActionResult About()
+        private void SearchInCategory(string query, ref List<Product> searchQuery)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            string norm_name;
+            foreach (var item in database.Categories)
+            {
+                norm_name = NormalizeDiacriticalCharacters(item.NameCate);
+                if (norm_name.Contains(query))
+                {
+                    foreach (var prod in item.Products)
+                    {
+                        if (!searchQuery.Contains(prod))
+                        {
+                            searchQuery.Add(prod);
+                        }
+                    }
+                }
+            }
         }
 
-        public ActionResult Contact()
+        public ActionResult Search(string query, int? page = 1)
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.CurrentPage = 0;
+            ViewBag.MaxPage = 0;
 
+            if (query == null || query.Length == 0)
+            {
+                return View();
+            }
+            query = NormalizeDiacriticalCharacters(query);
+            List<Product> searchQuery = new List<Product>();
+
+            string norm_name;
+            foreach (var item in database.Products)
+            {
+                norm_name = NormalizeDiacriticalCharacters(item.NamePro);
+                if (norm_name.Contains(query))
+                {
+                    searchQuery.Add(item);
+                }
+            }
+            SearchInCategory(query, ref searchQuery);
+
+            int maxPage = Math.Max(1, searchQuery.Count() / 10);
+            if (page > maxPage)
+            {
+                page = maxPage;
+            }
+            ViewBag.MaxPage = maxPage;
+            ViewBag.CurrentPage = page;
+            return View(searchQuery.Skip(((int)page - 1) * 10).Take(10));
+        }
+
+        private string NormalizeDiacriticalCharacters(string value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+
+            var normalised = value.Normalize(NormalizationForm.FormD).ToLower().ToCharArray();
+
+            return new string(normalised.Where(c => (int)c <= 127).ToArray());
+        }
+
+        public ActionResult ProductDetails()
+        {
             return View();
         }
     }
