ad79c463f13212691d62beb52cad437c59d207b0
Nguwuyn <22dh114652@st.huflit.edu.vn>
2024-11-04 18:21:19 +0700

    Revert "cloning"
    This reverts commit de96040ba035322dcb4a7b82f79b3adaa64d9f15.

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -1,91 +1,29 @@
-﻿using anhemtoicodeweb.Models;
-using anhemtoicodeweb.Models.ThongKe;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
-using System.Text;
+using System.Web;
 using System.Web.Mvc;
 
-namespace anhemtoicodeweb.Controllers
+namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly Model1 database = new Model1();
         public ActionResult Index()
         {
-            IEnumerable<Product> productList = database.Products.OrderByDescending(x => x.NamePro).ToList();
-            IEnumerable<Category> categoriesList = database.Categories.OrderByDescending(x => x.NameCate).ToList();
-            var tuple = new Tuple<IEnumerable<Product>, IEnumerable<Category>>(productList, categoriesList);
-            return View(tuple);
+            return View();
         }
 
-        private void SearchInCategory(string query, ref List<Product> searchQuery)
+        public ActionResult About()
         {
-            string norm_name;
-            foreach (var item in database.Categories)
-            {
-                norm_name = NormalizeDiacriticalCharacters(item.NameCate);
-                if (norm_name.Contains(query))
-                {
-                    foreach (var prod in item.Products)
-                    {
-                        if (!searchQuery.Contains(prod))
-                        {
-                            searchQuery.Add(prod);
-                        }
-                    }
-                }
-            }
+            ViewBag.Message = "Your application description page.";
+
+            return View();
         }
 
-        public ActionResult Search(string query, int? page = 1)
+        public ActionResult Contact()
         {
-            ViewBag.CurrentPage = 0;
-            ViewBag.MaxPage = 0;
+            ViewBag.Message = "Your contact page.";
 
-            if (query == null || query.Length == 0)
-            {
-                return View();
-            }
-            query = NormalizeDiacriticalCharacters(query);
-            List<Product> searchQuery = new List<Product>();
-
-            string norm_name;
-            foreach (var item in database.Products)
-            {
-                norm_name = NormalizeDiacriticalCharacters(item.NamePro);
-                if (norm_name.Contains(query))
-                {
-                    searchQuery.Add(item);
-                }
-            }
-            SearchInCategory(query, ref searchQuery);
-
-            int maxPage = Math.Max(1, searchQuery.Count() / 10);
-            if (page > maxPage)
-            {
-                page = maxPage;
-            }
-            ViewBag.MaxPage = maxPage;
-            ViewBag.CurrentPage = page;
-            return View(searchQuery.Skip(((int)page - 1) * 10).Take(10));
-        }
-
-        private string NormalizeDiacriticalCharacters(string value)
-        {
-            if (value == null)
-            {
-                throw new ArgumentNullException("value");
-            }
-
-            var normalised = value.Normalize(NormalizationForm.FormD).ToLower().ToCharArray();
-
-            return new string(normalised.Where(c => (int)c <= 127).ToArray());
-        }
-
-        public ActionResult ProductDetails()
-        {
             return View();
         }
     }
