75de42ffcb9f38bb9621acda6ab9634c5896390f
Phạm Trần Vĩnh An <22dh110050@st.huflit.edu.vn>
2024-11-17 23:48:41 +0700

    50 chu (2)

--- WebApplication1/Models/Category.cs
+++ WebApplication1/Models/Category.cs
@@ -18,7 +18,7 @@ namespace WebApplication1.Models
         public int CategoryID { get; set; }
 
         [Required]
-        [StringLength(100)]
+        [StringLength(50)]
         public string CategoryName { get; set; }
 
         public string CategoryIllust { get; set; }
